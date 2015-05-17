ab7d3db7288e0ed201ef9ae76feda41a53e8c060
theuchiha <mohdsanadzakirizvi@users.noreply.github.com>
2015-05-18 00:28:47 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,10 +2,10 @@ import requests
 from requests_oauthlib import OAuth1
 import json
 
-params={'q':'#ModiInChina','result':'mixed','until':'2015-05-18'}
-url = 'https://api.twitter.com/1.1/search/tweets.json'
+params={'q':'#ModiInChina','result':'mixed','until':'2015-05-18'}#add your query to 'q' key
+url = 'https://api.twitter.com/1.1/search/tweets.json'#using twitter search
 auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
-                  'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+                  'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')# add twitter oauth details
 r=requests.get(url, auth=auth,params=params)
 print r.status_code
 r=r.json()

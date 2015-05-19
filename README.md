e2332250065acf35fc1e0ee0614fa0b439e6da70
Mohd Sanad <mohdsanadzakirizvi@gmail.com>
2015-05-19 17:15:39 +0530

    updated code and readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Using Twitter Api to access tweets based on specific hashtags without using tweepy 
 
 Demo:
-[!alt tag](https://raw.github.com/mohdsanadzakirizvi/twitter-api/master/demo.png)
+[!alt tag](https://raw.github.com/mohdsanadzakirizvi/twitter_api/master/demo.png)rogra
+
+Instructions for setup:
+1. Make a twitter app
+2. Get your Access Tokens and Consumer Secret
+3. Update it into your python file
+4. Execute the program
+
+
--- twitter.py
+++ twitter.py
@@ -8,8 +8,7 @@ def main_func(q):#main function
 	current_time = date.today()#get the local date
 	url = 'https://api.twitter.com/1.1/search/tweets.json'
 	params = {'q' : q,'result' : 'mixed','until': current_time,'count' : limit}
-	auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
-                  'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+	auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET','USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')#add your auth details here
 	r = requests.get(url, auth = auth,params = params)
 	status = r.status_code
 	print status
